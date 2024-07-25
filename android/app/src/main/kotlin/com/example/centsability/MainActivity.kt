ec851ef6b957d532a555055489989f8ba0348730
Brandon McGhee <brandonmcghee@gmail.com>
2024-07-25 15:25:14 -0400

    harry potter

---
+++ android/app/src/main/kotlin/com/example/centsability/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.centsability
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/domain/entities/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+    final String name;
+    final double amount;
+    final DateTime date;
+    final int categoryId
+
+    Transaction({
+        required this.name,
+        required this.amount,
+        required this.date,
+        required this.categoryId
+    })
+}
